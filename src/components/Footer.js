fafcbe90efe89d06742d85b55f86950c159fd477
Amr-Magdy95 <37376273+Amr-Magdy95@users.noreply.github.com>
2023-01-02 21:34:09 +0200

    Feature: Added Sidebar HTML and CSS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,20 +21,19 @@ const Wrapper = styled.footer`
   align-items: center;
   background: var(--clr-black);
   text-align: center;
-  span{
+  span {
     color: var(--clr-primary-5);
   }
-  h5{
+  h5 {
     color: var(--clr-white);
     margin: 0.1rem;
     font-weight: 400;
     text-transform: none;
     line-height: 1.25;
   }
-  @media(min-width: 776px){
+  @media (min-width: 776px) {
     flex-direction: row;
   }
-
 `;
 
 export default Footer;
