ec7f59efba703fae072fef974349a48e5b457ca8
Amr-Magdy95 <37376273+Amr-Magdy95@users.noreply.github.com>
2023-01-03 12:53:41 +0200

    Feature: Added Error Page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 function ErrorPage() {
   return (
-    <div>ErrorPage</div>
-  )
+    <Wrapper className="page-100">
+      <section>
+        <h1>404</h1>
+        <h3>sorry, that page cannot be found</h3>
+        <Link to="/" className="btn">
+          back home
+        </Link>
+      </section>
+    </Wrapper>
+  );
 }
 
-export default ErrorPage
+const Wrapper = styled.main`
+  background: var(--clr-primary-10);
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  h1 {
+    font-size: 10rem;
+  }
+  h3 {
+    text-transform: none;
+    margin-bottom: 2rem;
+  }
+`;
+
+export default ErrorPage;
