2f65588c59d3a423e69346eff1c4503994a48b5a
Amr-Magdy95 <37376273+Amr-Magdy95@users.noreply.github.com>
2023-01-15 20:58:14 +0200

    Feature: Added filtered products context

---
+++ src/context/filter_context.js
@@ -0,0 +1,33 @@
+import React,{ useContext, useEffect, useReducer } from "react";
+import reducer from '../reducers/filter_reducer'
+import {
+  LOAD_PRODUCTS,
+  SET_GRIDVIEW,
+  SET_LISTVIEW,
+  UPDATE_SORT,
+  SORT_PRODUCTS,
+  UPDATE_FILTERS,
+  FILTER_PRODUCTS,
+  CLEAR_FILTERS,
+} from '../actions'
+import { useProductsContext } from './products_context'
+
+const initialState = {
+  filtered_products: [],
+  all_products: []
+}
+
+const FilterContext = React.createContext();
+
+export const FilterContextProvider = ({children})=>{
+    const {products} = useProductsContext()
+    useEffect( ()=>{
+      dispatch({type: LOAD_PRODUCTS, payload: products});
+    }, [products])
+    const [state, dispatch] = useReducer( reducer, initialState);
+    return (<FilterContext.Provider value="hello">{children}</FilterContext.Provider>)
+}
+
+export const useFilterContext = () =>{
+    return useContext(FilterContext);
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ProductsProvider } from "./context/products_context";
+import { FilterContextProvider } from "./context/filter_context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ProductsProvider>
-    <App />
+    <FilterContextProvider>
+      <App />
+    </FilterContextProvider>
   </ProductsProvider>
 );
 
